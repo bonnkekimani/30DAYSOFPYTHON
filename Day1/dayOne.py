08dd63be8e5fd518a83316cb5feadbd1f4b7fddd
bonnkekimani <bonnke.kimani@student.moringaschool.com>
2023-05-15 23:58:11 +0300

    Day1

--- Day1/dayOne.py
+++ Day1/dayOne.py
@@ -12,7 +12,7 @@ def PythonCalculator():
     numOne= int(input("Please Enter number 1: "))
     numTwo= int(input("Please Enter number 2: "))
 
-    operand = int(input("Enter the Operand '\n' 1:add '\n' 2:subtract '\n' 3:multiply,'\n' 4:divide: '\n' "))
+    operand = int(input("Enter the Operand '\n' 1:Add '\n' 2:Subtract '\n' 3:Multiply,'\n' 4:Divide: '\n' 5:Deletion,'\n' 6:Power,'\n' 7:SquareRoot,'\n' "))
 
     if operand ==1:
         return numOne + numTwo
@@ -21,7 +21,13 @@ def PythonCalculator():
     elif operand ==3:
         return numOne * numTwo
     elif operand == 4:
-        return numOne + numTwo
+        return numOne // numTwo
+    elif operand ==5:
+        return numOne * numTwo
+    elif operand ==6:
+        return numOne ** numTwo
+    elif operand ==7:
+        return (numOne or numTwo) ** 0.5
     else:
         return "You entered the wrong operand"
 
